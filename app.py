50df49962c9a0f42b08f8faaf9c6dee0881d99a2
Luke <1184185682@qq.com>
2026-04-03 18:44:26 +0800

    fix(deploy): adapt for render.com deployment by using PORT env var and adding gunicorn

--- app.py
+++ app.py
@@ -720,4 +720,5 @@ def download_file(filename):
     return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=False)
